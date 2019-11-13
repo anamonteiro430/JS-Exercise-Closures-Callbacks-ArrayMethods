878e2c05b780a8bf344d7ad4584b0b9df7bc48db
kika <anamonteiro430@gmail.com>
2019-11-13 15:17:21 +0000

    shirt size

--- index.js
+++ index.js
@@ -228,8 +228,10 @@ function firstNamesAllCaps(runners,i) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  return runners.filter(function(shirt){
+    return shirt.shirt_size === tShirtSize;
+  })
 }
 
 /**
